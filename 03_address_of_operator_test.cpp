be4f28f115fe61377af210f7ea941d5461da58d5
unknown <galilburuk@gmail.com>
2026-07-03 20:51:37 +0300

    test: evaluate memory addresses of varios expressions using the address-of operator

---
+++ 03_address_of_operator_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int num = 12;
+    std::cout<<"Address of num = "<<&num<<std::endl;
+
+    //std::cout<<&(num+5)<<'\n';        // We are getting error message because "num+5" is a rvalue and doesn't have an address.
+    //std::cout<<&("Hello")<<'\n';      //This works properly string literals have memory addresses.
+    //std::cout<<&(++num)<<'\n';        //This is also fine with pre-increment we still have the lvalue.
+    //std::cout<<&(num++)<<'\n';        //Unlike previos example post-incement operator gives copy memoryless rvalue.
+    //std::cout<<&(num*=2)<<'\n';       //This works fine operators like "+=", "-=", "*=" gives lvalue, updated version of num but still lvalue.
+    return 0;
+}
