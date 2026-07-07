6afc2da8eec2eca667abd8f62721d30a03780f5f
Halil İbrahim Demir <galilburuk@gmail.com>
2026-07-07 16:02:44 +0300

    Update 03_address_of_operator_test.cpp

--- 03_address_of_operator_test.cpp
+++ 03_address_of_operator_test.cpp
@@ -4,7 +4,7 @@ int main(){
     int num = 12;
     std::cout<<"Address of num = "<<&num<<std::endl;
 
-    //std::cout<<&(num+5)<<'\n';        // We are getting error message because "num+5" is a rvalue and doesn't have an address.
+    //std::cout<<&(num+5)<<'\n';        //We are getting error message because "num+5" is a rvalue and doesn't have an address.
     //std::cout<<&("Hello")<<'\n';      //This works properly string literals have memory addresses.
     //std::cout<<&(++num)<<'\n';        //This is also fine with pre-increment we still have the lvalue.
     //std::cout<<&(num++)<<'\n';        //Unlike previos example post-incement operator gives copy memoryless rvalue.
