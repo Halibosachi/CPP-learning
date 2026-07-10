6540a6003cb552414d789b5622285e5357124d40
unknown <galilburuk@gmail.com>
2026-07-10 20:36:56 +0300

    test: verify const reference protection on fundamental data types

---
+++ 16_const_ref_fundamental_type.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+void weightControl(const double& weight){
+    //weight = 85.2;                            //const forbids us to change value of weight.
+}
+
+int main(){
+    double current_weight = 80.0;
+
+    weightControl(current_weight);
+
+
+
+    return 0;
+}
