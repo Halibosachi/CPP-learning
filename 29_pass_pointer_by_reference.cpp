8d140a76264738a1b165ab240ecb2706b53e65b9
Halil <galilburuk@gmail.com>
2026-07-21 15:40:37 +0300

    feat: implement pass pointer by reference for dynamic target switching mechanic

---
+++ 29_pass_pointer_by_reference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void changeTarget(int*& target_ptr, int* new_target_address){                               //Changing pointer's value with pointer by reference.
+    target_ptr = new_target_address;                                                        //Redirecting the soriginal pointer to point to the new enemy's memory address.
+    std::cout << "Target locked to a new enemy!\n";
+}
+
+int main() {
+    int enemy1_hp { 100 };
+    int enemy2_hp { 200 };
+
+    int* current_target_ptr { &enemy1_hp };
+    std::cout << "Shooting at enemy with HP: " << *current_target_ptr << '\n';
+
+    changeTarget(current_target_ptr, &enemy2_hp);
+
+    std::cout << "Shooting at enemy with HP: " << *current_target_ptr << '\n';
+    
+    return 0;
+}
