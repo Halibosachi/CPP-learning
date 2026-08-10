dd990c35e4ff4e32da54b01cd301ff9358178deb
Halil <galilburuk@gmail.com>
2026-08-10 17:56:19 +0300

    docs: clarify implicit string conversion and const reference binding mechanics for rvalues

--- 15_const_ref_string_literals.cpp
+++ 15_const_ref_string_literals.cpp
@@ -9,7 +9,10 @@ int main(){
     std::string m_name = "Airplane!";
     printMesssage(m_name);
 
-    printMesssage("Spirited Away");     //This would have given error message if we didn't use const pass by reference method.        
+    // Note: "Spirited Away" is a string literal (lvalue const char[]).
+    // But function expects std::string thats why it creates a copy of "Spirited Away" as a rvalue.
+    // If we didn't use 'const' on the function we would get an error because temporary rvalues can't bind to non-const references.
+    printMesssage("Spirited Away");        
 
 
 
