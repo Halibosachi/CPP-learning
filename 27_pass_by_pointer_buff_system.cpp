28adcdb33217718f3fef69e3c9bf633e692117f3
Halil <galilburuk@gmail.com>
2026-08-11 16:33:40 +0300

    docs: refine inline comments for pointer dereferencing

--- 27_pass_by_pointer_buff_system.cpp
+++ 27_pass_by_pointer_buff_system.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 void applyBuff(int* stats_ptr){
-    if(!stats_ptr){                                                                 //Null pointer check.
+    if(!stats_ptr){                                                                 // Null pointer check.
         std::cout << "No target selected. Buff wasted." << '\n';
     } else {
-        *stats_ptr += 25;                                                           //With dereference changing the value of "warrior_strength".
+        *stats_ptr += 25;                                                           // With dereference changing the value of "warrior_strength".
         std::cout << "Buff applied! New stats: " << *stats_ptr << '\n';
     }
 }
@@ -14,11 +14,11 @@ int main() {
     int warrior_strength{ 100 };
     int* invalid_ptr{};
 
-    applyBuff(&warrior_strength);                                                   //This line works fine.
+    applyBuff(&warrior_strength);                                                   // Inside the function we changed the value of "warrior_strength".
 
-    std::cout << "New value of warrior_strength: " << warrior_strength << '\n';     //Inside the function we changed the value of "warrior_strength".
+    std::cout << "New value of warrior_strength: " << warrior_strength << '\n';     // Showing the new value of "warrior_strength".
 
-    applyBuff(invalid_ptr);                                                         //This line is safe! It won't crash because we handled the nullptr case inside the function. 
+    applyBuff(invalid_ptr);                                                         // This line is safe! It won't crash because we handled the nullptr case inside the function. 
 
     return 0;
 }
